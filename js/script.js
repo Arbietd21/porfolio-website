e046d1bb350ba1fb5cc77faef3053d1cf31c85b5
Arbietd21 <arbiealemany14@gmail.com>
2023-10-26 01:18:41 -0500

    added hamburger button

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let hamburgerBtn = document.querySelector('#hambuger')
+let hamburgerBar = document.querySelectorAll('.bar')
+
+function isActive(element) {
+    element.classList.add('is-active')
+}
+
+console.log(hamburgerBtn)
